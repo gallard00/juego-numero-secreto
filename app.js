00a9678b9e11dcb85613349d6d3640fd3dc7bcc8
Nahuel <nahuel.ng240@gmail.com>
2025-01-29 08:24:24 -0300

    agregando funciones en js

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+let parrafo = document.querySelector('p');
+
+parrafo.innerHTML = "Elija un numero del 1 al 10";
+titulo.innerHTML = "Bienvenido al juego del numero secreto";
+
+function intentoDeUsuario() {
+    alert('Click desde la funcion');
+}
